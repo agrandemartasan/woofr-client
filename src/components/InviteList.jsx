1f7202841d11880db0f2d97ebfb4b3f0201bd1af
Marta <martinha_peres@hotmail.com>
2023-03-10 16:17:03 +0000

    invites list styling

--- src/components/InviteList.jsx
+++ src/components/InviteList.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { acceptInvite, getInvitesReceived, rejectInvite } from "../api";
 import { UserContext } from "../context/user.context";
+import { Box, Button, Text, useColorModeValue } from "@chakra-ui/react";
 
 function InviteList() {
   const { loggedUser } = useContext(UserContext);
@@ -33,26 +34,79 @@ function InviteList() {
   }
 
   return (
-    <div>
-      <h2>Invites</h2>
-      <ul>
+    <Box
+      d="flex"
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      bg={useColorModeValue("brand.50", "brand.600")}
+      w={{ base: "100%", md: "31%" }}
+      h="100%"
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+        pb={1}
+        px={3}
+        fontSize={{ base: "28px", md: "30px" }}
+        d="flex"
+        w="100%"
+        justifyContent="space-between"
+        alignItems="center"
+        color={useColorModeValue("brand.700", "brand.200")}
+      >
+        Invites
+      </Box>
+      <Box
+        d="flex"
+        flexDir="column"
+        p={3}
+        bg={useColorModeValue("brand.50", "brand.600")}
+        color={useColorModeValue("brand.50", "brand.900")}
+        w="100%"
+        h="100%"
+        borderRadius="lg"
+        overflowY="hidden"
+      >
         {invites.length > 0 ? (
           invites.map((invite) => (
-            <li key={invite._id}>
-              {invite.sender.username} wants to be your friend!
-              <button onClick={() => handleAcceptInvite(invite._id)}>
+            <Box
+              bg="brand.400"
+              px={3}
+              py={2}
+              borderRadius="lg"
+              key={invite._id}
+            >
+              <Text fontSize="lg" pb={2}>
+                {invite.sender.username} wants to be your friend!
+              </Text>
+              <Button
+                colorScheme={"brand"}
+                mr={2}
+                onClick={() => handleAcceptInvite(invite._id)}
+              >
                 Accept
-              </button>
-              <button onClick={() => handleRejectInvite(invite._id)}>
+              </Button>
+              <Button
+                colorScheme={"brand"}
+                onClick={() => handleRejectInvite(invite._id)}
+              >
                 Reject
-              </button>
-            </li>
+              </Button>
+            </Box>
           ))
         ) : (
-          <li>No invites yet.</li>
+          <Box
+            bg="brand.400"
+            p={3}
+            borderRadius="lg"
+            fontSize={{ base: "sm", md: "md" }}
+          >
+            No invites yet.
+          </Box>
         )}
-      </ul>
-    </div>
+      </Box>
+    </Box>
   );
 }
 
