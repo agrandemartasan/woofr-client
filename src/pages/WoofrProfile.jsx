a0489eba762f4851da0383e95d0846805a1360ba
Marta <martinha_peres@hotmail.com>
2023-03-11 11:31:56 +0000

    added add friend button to WoofrProfile

--- src/pages/WoofrProfile.jsx
+++ src/pages/WoofrProfile.jsx
@@ -13,10 +13,14 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getUser } from "../api";
 import { UserContext } from "../context/user.context";
+import { sendInvite, unfriendUser } from "../api";
 
 function WoofrProfile() {
   const { loggedUser } = useContext(UserContext);
   const { userId } = useParams();
+
+  const [loggedUserData, setLoggedUserData] = useState({});
+  const [userFriendshipStatus, setUserFriendshipStatus] = useState("");
   const [username, setUsername] = useState("");
   const [profilePicture, setProfilePicture] = useState("");
   const [location, setLocation] = useState("");
@@ -30,6 +34,42 @@ function WoofrProfile() {
   const [isVaccinated, setIsVaccinated] = useState(false);
   const [isTrained, setIsTrained] = useState(false);
   const [size, setSize] = useState("");
+
+  function checkFriendshipStatus(userId) {
+    //Todo: check for invites from other users
+    const isInvited = (userId) => {
+      return loggedUserData.invitesSent?.includes(userId);
+    };
+
+    const isFriend = (userId) => {
+      return loggedUserData.friends?.some((friend) => friend._id === userId);
+    };
+
+    if (isInvited(userId)) return "InviteSent";
+    if (isFriend(userId)) return "RemoveFriend";
+
+    return "AddFriend";
+  }
+  async function handleAddFriend(friendId) {
+    try {
+      const newInvite = await sendInvite({
+        sender: loggedUser._id,
+        recipient: friendId
+      });
+      setUserFriendshipStatus("InviteSent");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleRemoveFriend(friendId) {
+    try {
+      await unfriendUser(loggedUser, friendId);
+      setUserFriendshipStatus("RemoveFriend");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
     async function handleGetUserDetails() {
@@ -47,121 +87,173 @@ function WoofrProfile() {
       setSize(response.data.info.size);
     }
 
+    async function getOwnUserDetails() {
+      if (loggedUser) {
+        const loggedUserResponse = await getUser(loggedUser._id);
+        setLoggedUserData(loggedUserResponse.data);
+        console.log("loggedUserResponse.data", loggedUserResponse.data);
+      }
+    }
+
     handleGetUserDetails(userId);
-  }, [userId]);
+    getOwnUserDetails();
+    setUserFriendshipStatus(checkFriendshipStatus(userId));
+  }, [userId, loggedUser]);
 
   return (
     <>
-      {loggedUser._id !== userId && <Button>Add Friend</Button>}
       <Flex
-        flexDirection={{ base: "column", md: "row" }}
+        flexDirection="column"
         alignItems="flex-start"
-        justifyContent="center"
+        justifyContent="flex-start"
         w="100%"
         minH="91vh"
-        gap={4}
-        py={7}
-        px={5}
+        py={3}
         bg={useColorModeValue("brand.100", "brand.850")}
       >
-        <Box
-          p={3}
-          bg={useColorModeValue("brand.50", "brand.600")}
-          w={{ base: "100%", md: "27%" }}
-          borderRadius="lg"
-          borderWidth="1px"
-        >
-          <Image
-            src={profilePicture}
-            alt={username}
-            w="100%"
-            borderRadius="lg"
-            borderWidth="1px"
-          />
-        </Box>
         <Flex
-          flexDirection="column"
-          p={3}
-          bg={useColorModeValue("brand.50", "brand.600")}
-          w={{ base: "100%", md: "50%" }}
-          h="100%"
-          borderRadius="lg"
-          borderWidth="1px"
+          alignItems="flex-end"
+          justifyContent="flex-end"
+          w="100%"
+          px={"5%"}
+          bg={useColorModeValue("brand.100", "brand.850")}
+        >
+          {userFriendshipStatus && (
+            <Button
+              onClick={
+                userFriendshipStatus === "RemoveFriend"
+                  ? () => handleRemoveFriend(userId)
+                  : userFriendshipStatus === "AddFriend"
+                  ? () => handleAddFriend(userId)
+                  : null
+              }
+              w={"150px"}
+              my={2}
+              fontSize={"sm"}
+              rounded={"full"}
+              bg={"brand.400"}
+              color={"brand.50"}
+              _hover={{
+                bg: "brand.500"
+              }}
+              _focus={{
+                bg: "brand.500"
+              }}
+            >
+              {userFriendshipStatus === "InviteSent" && "Invite Sent"}
+              {userFriendshipStatus === "RemoveFriend" && "Remove Friend"}
+              {userFriendshipStatus === "AddFriend" && "Add Friend"}
+            </Button>
+          )}
+        </Flex>
+        <Flex
+          alignItems="flex-start"
+          justifyContent="center"
+          flexDirection={{ base: "column", md: "row" }}
+          w="100%"
+          gap={4}
+          px={"5%"}
         >
           <Box
-            pb={3}
-            px={3}
-            fontSize={{ base: "28px", md: "30px" }}
-            d="flex"
-            w="100%"
-            color={useColorModeValue("brand.700", "brand.200")}
-          >
-            Hi! My name is {username}!
-          </Box>
-          <Box
-            m={"auto"}
             p={3}
             bg={useColorModeValue("brand.50", "brand.600")}
-            w="100%"
+            w={{ base: "100%", md: "30%" }}
+            borderRadius="lg"
+            borderWidth="1px"
+          >
+            <Image
+              src={profilePicture}
+              alt={username}
+              w="100%"
+              borderRadius="lg"
+              borderWidth="1px"
+            />
+          </Box>
+          <Flex
+            flexDirection="column"
+            p={3}
+            bg={useColorModeValue("brand.50", "brand.600")}
+            w={{ base: "100%", md: "70%" }}
             h="100%"
             borderRadius="lg"
             borderWidth="1px"
           >
-            <Text>I'm from {location}</Text>
-            <Text>
-              I was born {formatDistanceToNow(new Date(birthday))} ago
-            </Text>
-            <Text>About me: {bio}</Text>
-          </Box>
-          <Box
-            m={"auto"}
-            p={3}
-            bg={useColorModeValue("brand.50", "brand.600")}
-            w="100%"
-            h="100%"
-            borderRadius="lg"
-            borderWidth="1px"
-          >
-            <Text>My gender is: {gender}</Text>
-            <Text>My breed is: {breed}</Text>
-            <Text>My size is: {size}</Text>
-          </Box>
-          <HStack
-            m={"auto"}
-            p={3}
-            bg={useColorModeValue("brand.50", "brand.600")}
-            w="100%"
-            h="100%"
-            borderRadius="lg"
-            borderWidth="1px"
-          >
-            <Tag
-              size={{ base: "md", md: "lg" }}
-              py={2}
-              px={4}
-              colorScheme={"brand"}
+            <Box
+              pb={3}
+              px={3}
+              fontSize={{ base: "28px", md: "30px" }}
+              d="flex"
+              w="100%"
+              color={useColorModeValue("brand.700", "brand.200")}
             >
-              {isNeuteredOrSpayed
-                ? "I'm neutered/spayed"
-                : "I'm not neutered/spayed"}
-            </Tag>
-            <Tag
-              size={{ base: "md", md: "lg" }}
-              py={2}
-              px={4}
-              colorScheme={"brand"}
+              Hi! My name is {username}!
+            </Box>
+
+            <Box
+              m={"auto"}
+              p={3}
+              bg={useColorModeValue("brand.50", "brand.600")}
+              w="100%"
+              h="100%"
+              borderRadius="lg"
+              borderWidth="1px"
             >
-              {isVaccinated ? "I'm vaccinated" : "I'm not vaccinated"}
-            </Tag>
-            <Tag
-              size={{ base: "md", md: "lg" }}
-              py={2}
-              px={4}
-              colorScheme={"brand"}
+              <Text>I'm from {location}</Text>
+              <Text>
+                I was born {formatDistanceToNow(new Date(birthday))} ago
+              </Text>
+              <Text>About me: {bio}</Text>
+            </Box>
+            <Box
+              m={"auto"}
+              p={3}
+              bg={useColorModeValue("brand.50", "brand.600")}
+              w="100%"
+              h="100%"
+              borderRadius="lg"
+              borderWidth="1px"
             >
-              {isTrained ? "I'm trained" : "I'm not trained"}
-            </Tag>
-          </HStack>
+              <Text>My gender is: {gender}</Text>
+              <Text>My breed is: {breed}</Text>
+              <Text>My size is: {size}</Text>
+            </Box>
+            <HStack
+              m={"auto"}
+              p={3}
+              bg={useColorModeValue("brand.50", "brand.600")}
+              w="100%"
+              h="100%"
+              borderRadius="lg"
+              borderWidth="1px"
+            >
+              <Tag
+                size={{ base: "md", md: "lg" }}
+                py={2}
+                px={4}
+                colorScheme={"brand"}
+              >
+                {isNeuteredOrSpayed
+                  ? "I'm neutered/spayed"
+                  : "I'm not neutered/spayed"}
+              </Tag>
+              <Tag
+                size={{ base: "md", md: "lg" }}
+                py={2}
+                px={4}
+                colorScheme={"brand"}
+              >
+                {isVaccinated ? "I'm vaccinated" : "I'm not vaccinated"}
+              </Tag>
+              <Tag
+                size={{ base: "md", md: "lg" }}
+                py={2}
+                px={4}
+                colorScheme={"brand"}
+              >
+                {isTrained ? "I'm trained" : "I'm not trained"}
+              </Tag>
+            </HStack>
+          </Flex>
         </Flex>
       </Flex>
     </>
